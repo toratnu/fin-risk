eeb26de2813a5cce3d04290dc1615d6ab7380794
toratanu <yimlcom@live.jp>
2025-07-31 22:36:51 +0900

    Fix: Use fileURLToPath for __dirname in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,7 @@ export default defineConfig({
   base: './',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), './src'),
     },
   },
 });
